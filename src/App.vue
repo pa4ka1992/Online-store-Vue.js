3873d2ea23b5d51f0e9f29115bc1bae9b47543fd
Nikita Kisly <sour945@gmail.com>
2022-12-14 10:36:51 +0300

    refactor: change webpack config structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
+<template> 
+  <h1 class="header">11111</h1>
+  <div> 
+    <RouterView/>
+  </div>
+  
+  </template>
+
 <script lang="ts" setup>
 </script>
 
-<template> 
-
-<div> 
-  <RouterView/>
-</div>
-
-</template>
+<style scoped lang="scss">
+  .header {
+    color: red;
+  }
+</style>

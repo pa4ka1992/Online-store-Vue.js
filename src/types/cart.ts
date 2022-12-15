156b5761fca952723c0d3780dbb3c20475841a6b
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-15 10:45:16 +0500

    feat: add methods to cart

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -3,4 +3,7 @@ import { Product } from "./product";
 export interface Cart {
   get totalPrice(): number;
   get products(): Product[];
+  addProduct(product: Product): void;
+  removeProduct(product: Product): void;
+  applyDiscountCode(code: string): boolean;
 }

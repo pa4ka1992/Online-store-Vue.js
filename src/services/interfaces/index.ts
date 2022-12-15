7d5eaa2279f1aa91d51d806fabb0ac64a2991a86
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-15 20:09:44 +0500

    refactor: remove redudant files

--- src/services/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-export { ICart } from './cart';
-export { Filter } from './filter';
-export { IProduct } from './product';
-export { IProductStore } from './product-store';
--- src/services/interfaces/product-store.ts
+++
@@ -1,9 +0,0 @@
-import { IProduct } from './product';
-import { Filter } from './filter';
-import { Observable } from '@/common/observable';
-
-export interface IProductStore {
-  get products(): Observable<IProduct[]>;
-  applyFilters(filters: Filter[]): void;
-  getProductById(id: IProduct['id']): IProduct;
-}

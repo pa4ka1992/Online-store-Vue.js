607f4bf72c03a9cab9e5a056e1ab345551b35bd5
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-15 12:55:59 +0500

    feat: add pinia plugin to vue

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue';
 // import './scss/index.scss';
 import router from './router';
 import App from './App.vue';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
+app.use(pinia);
 app.mount('#app');

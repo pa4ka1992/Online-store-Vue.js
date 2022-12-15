517672efc139fce1aeec33f1e32d7f5afdcc3d17
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-15 10:40:37 +0500

    refactor: refactor filter due to YAGNI

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -1,18 +1,3 @@
 import { Product } from './product';
 
-// Pass to generic option you want to filter by 
-
-export type FilterOptions<Key extends keyof Product> = {
-  filterValue: Product[Key];
-
-  // Only options for strings
-  seekValue: Product[Key] extends string ? string : undefined;
-
-  // Only options for numbers
-  upperValue: Product[Key] extends number ? number : undefined;
-  lowerValue: Product[Key] extends number ? number : undefined;
-}
-
-export interface FilterItem {
-  <Key extends keyof Product>(item: Product, key: Key, options: FilterOptions<Key>): boolean;
-}
+export type Filter = (product: Product) => boolean;

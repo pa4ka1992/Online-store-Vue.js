43fb37ec8d027f4facaf7939fbb2d384a9d2227f
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-16 18:23:25 +0500

    refactor: fix name

--- src/services/filters/equal-filter.ts
+++ src/services/filters/equal-filter.ts
@@ -1,7 +1,7 @@
 import { IFilter } from '../model/filter';
 import { IProduct } from '../model/product';
 
-export function useSearchFilter<Key extends keyof IProduct>(key: Key, item: IProduct[Key]): IFilter {
+export function useEqualFilter<Key extends keyof IProduct>(key: Key, item: IProduct[Key]): IFilter {
   return function (product: IProduct) {
     return item === product[key];
   };

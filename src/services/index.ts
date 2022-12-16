d0068c7a0363c6a91705e0f470fc81bd1baae002
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-16 18:24:22 +0500

    refactor: add index file

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+export { LocalStorageApi } from './local-storage';
+export { ProductRepository } from './product.repository';
+export { IProduct } from './model/product';
+export { IFilter } from './model/filter';
+export { useEqualFilter } from './filters/equal-filter';
+export { useSearchFilter } from './filters/search-filter';
+export { useRangeFilter } from './filters/range-filter';

f0b93f935c37e0980772a94b333dbdf3a452035e
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-16 16:18:49 +0500

    feat: add get product by id and  filters

--- src/store/products.store.ts
+++ src/store/products.store.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import { ref, type Ref, computed } from 'vue';
 
+import { Filter } from './interfaces/filter';
 import { IProduct } from './interfaces/product';
 import { ProductRepository } from '@/services/product.repository';
 
@@ -8,19 +9,29 @@ import { ProductRepository } from '@/services/product.repository';
 export const useProductsStore = defineStore('products', () => {
   const _products: Ref<IProduct[]> = ref([]);
   const repo = ProductRepository.getInstance();
+  const _productMap: Ref<{[key: string]: IProduct}> = ref({});
 
   async function fetchData() {
     _products.value = await repo.fetchProducts();
+    _products.value.forEach((item) => _productMap.value[item.id] = item);
   }
 
   fetchData();
 
   const products = computed(() => {
-    return [..._products.value];
-  })
+    return _products.value.filter((product) => {
+      return filters.value.reduce((acc, item) => acc && item(product), true);
+    });
+  });
+
+  function getProductById(id: string) {
+    return _productMap.value[id];
+  }
+
+  const filters: Ref<Filter[]> = ref([]);
 
   return {
-    fetchData,
     products,
+    getProductById
   };
 });

4ffb5c9fbe1fda8eac16a822ca147d5ba040402d
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-16 18:13:41 +0500

    refactor: add products store to index file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,3 @@
 export { usePinia } from './pinia';
 export { useTestStore } from './test-store';
-export { Filter } from './interfaces/filter';
-export { IProduct } from './interfaces/product';
+export { useProductsStore } from './products.store';

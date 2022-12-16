3ba5ee7b18a38c195d96cafd5cf1b0fe6a44b9a1
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-16 16:26:00 +0500

    feat: add generic constructor to repository

--- src/services/product.repository.ts
+++ src/services/product.repository.ts
@@ -7,11 +7,13 @@ export class ProductRepository {
     // It has to be private (see singleton)
   }
 
+  static setInstance<Type extends IProductSource>(ctor: { new (): Type }): IProductSource {
+    ProductRepository.instance = new ctor();
+    return ProductRepository.instance; 
+  };
+
   static getInstance(): IProductSource {
-    if (ProductRepository.instance === null) {
-      throw new Error('Not implemented');
-      // ProductRepository.instance = new Type();
-    }
+    if (ProductRepository.instance === null) throw new Error('Product source was not defined!'); 
     return ProductRepository.instance;
   }
 }

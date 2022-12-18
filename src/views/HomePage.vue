28f5e0b968c5df9566d0ec661e94327ba8271ad3
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-18 14:32:43 +0500

    feat: add double range component

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,13 @@
 <script lang="ts" setup>
 
+import DoubleRangeSlider from '@/components/DoubleRangeSlider.vue';
 import { useTestStore } from '../store';
+import { ref } from 'vue';
 
 const store = useTestStore();
+
+const left = ref(0);
+const right = ref(50);
 
 </script>
 
@@ -11,6 +16,7 @@ const store = useTestStore();
 <div> 
   <h1> Counter is {{store.count}}</h1>
   <button class="btn btn-primary" @click="store.increment()">Click me!</button>
+  <double-range-slider :step=10 v-model:left="left" v-model:right="right"></double-range-slider>
 </div>
 
 </template>

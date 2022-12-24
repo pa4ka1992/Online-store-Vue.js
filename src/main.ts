3beafa97cd4f1703604505a8f1d94c23813b0168
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-24 13:55:31 +0500

    refactor: move global components to plugins

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,13 @@ import './assets/scss/index.scss';
 import './assets/font-icons/css/custom-pack.css';
 import router from './router';
 import App from './App.vue';
+import { commonPlugin, fontAwesomePlugin } from './plugins';
 import { usePinia } from './store';
-import components from '@/components/UI'
-import { apllyAwesome } from '@/services/awesome'
 
 export const app = createApp(App);
 
 app.use(router)
-.use(usePinia())
-.mount('#app');
-
-components.forEach((component) => {
-  app.component(component.name, component)
-})
-
-apllyAwesome(app)
-
-
-
+   .use(usePinia())
+   .use(commonPlugin)
+   .use(fontAwesomePlugin)
+   .mount('#app');

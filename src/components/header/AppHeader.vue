7a9bc998fa11dd1d94f61852ff7a2df355c60ca5
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-26 20:56:52 +0500

    feat: add header

---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+
+import SearchBar from './SearchBar.vue';
+import CartLink from './CartLink.vue';
+
+import { ref } from 'vue';
+
+const val = ref("");
+
+function click() {
+  console.log('Gotcha!', val.value);
+}
+
+</script>
+
+<template> 
+  <header class="page-header">
+    <RouterLink class="a" to="/"> 
+      <div class="logo">
+        Online Store
+      </div> 
+    </RouterLink>
+    <SearchBar @search="click" v-model="val"/>
+    <CartLink/>
+  </header>
+</template>
+
+<style scoped lang="scss">
+
+@import '@/assets/scss/variables.scss'; 
+
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+.page-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 35px;
+  background-color: $primary-dark;
+  color: $white;
+
+  @include apply-shadow;
+}
+
+.logo {
+  user-select: none;  
+  font-size: 2rem;
+  font-family: 'Pacifico', cursive;
+
+  transition: scale 0.5s;
+
+  &:hover {
+    scale: 1.2;
+  }
+}
+
+</style>

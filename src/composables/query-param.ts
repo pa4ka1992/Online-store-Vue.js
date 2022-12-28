ec8427a9450b20d1ede3b1d7513045539f84858a
Cyrus <cyrus.kuznetsov@gmail.com>
2022-12-28 15:15:58 +0500

    feat: query composable now support num arrays

--- src/composables/query-param.ts
+++ src/composables/query-param.ts
@@ -1,10 +1,10 @@
 import { computed } from 'vue';
 import { useRouter } from 'vue-router';
-import { isStringArray, isNumber, isString } from '@/utils';
+import { isStringArray, isNumberArray, isString } from '@/utils';
 
 const urlSeparator = ';';
 
-export type TQueryParameter = number | string[] | null;
+export type TQueryParameter = number[] | string[] | null;
 
 export function useQueryParam(key: string) {
   const router = useRouter();
@@ -12,14 +12,17 @@ export function useQueryParam(key: string) {
   const param = computed({
     get() {
       const queryParam = router.currentRoute.value.query[key];
-      if (isNumber(Number(queryParam))) return Number(queryParam);
-      else if (isString(queryParam)) return queryParam.split(urlSeparator);
+      if (isString(queryParam)) { 
+        const array = queryParam.split(urlSeparator);
+        if (isNumberArray(array)) return array.map((value) => Number(value));
+        else return array;
+      }
       else return null;
     },
     set(value) {
       let setValue: number | string | undefined = undefined;
 
-      if (isNumber(value)) setValue = value;
+      if (isNumberArray(value)) setValue = value.join(urlSeparator);
       else if (isStringArray(value)) setValue = value.join(urlSeparator);
 
       router.push({

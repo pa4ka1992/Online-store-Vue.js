5eb3eaf6c8f1831a77f59752287a44de95810581
Cyrus <cyrus.kuznetsov@gmail.com>
2023-01-01 21:59:09 +0500

    fix: fix footer and add year

--- src/components/footer/AppFooter.vue
+++ src/components/footer/AppFooter.vue
@@ -2,6 +2,10 @@
 
 import GithubLink from './GithubLink.vue';
 
+import { ref } from 'vue';
+
+const year = ref((new Date()).getFullYear());
+
 </script>
 
 <template> 
@@ -9,6 +13,10 @@ import GithubLink from './GithubLink.vue';
     <a class="a course-link" href="https://rs.school/js">
       <img class="course-link__logo" src="https://rs.school/images/rs_school_js.svg"/>
     </a>
+    <span class="year">
+      @{{year}}.
+      All rights reserved
+    </span>
     <address class="authors-section">
       <h4 class="authors-section__heading">Website created by:</h4>
       <GithubLink name="RichGihratik"/>
@@ -22,24 +30,31 @@ import GithubLink from './GithubLink.vue';
 @import '@/assets/scss/variables.scss';
 
 .page-footer {
-  @include apply-shadow;
   background-color: $primary2;
   color: $black;
   display: flex;
   justify-content: space-between;
-  align-items: flex-end;
+  align-items: center;
+  padding: 20px;
 }
 
 .course-link {
-  margin: 20px;
   &__logo {
     width: 160px;
     height: 80px;
   }
 }
 
+.year {
+  font-size: 1.5rem;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 300;
+  align-self: center;
+  text-align: center;
+}
+
 .authors-section {
-  padding: 20px;
   display: flex;
   flex-direction: column;
   align-items: flex-start;

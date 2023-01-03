910251ccc52af058d9c3f42f7cf5961e2db99def
Cyrus <cyrus.kuznetsov@gmail.com>
2023-01-03 12:29:12 +0500

    feat: add separate discount service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 export { LocalStorageApi } from './local-storage';
 export { ProductRepository } from './product.repository';
+export * from './discount';
 export * from './model';
 export * from './filters';
 export * from './sort';

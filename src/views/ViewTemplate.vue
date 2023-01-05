c2b0f27d9d350c7280bf1417f62e6afdb565800a
Cyrus <cyrus.kuznetsov@gmail.com>
2023-01-05 20:14:21 +0500

    feat: create component for route transitions

---
+++ src/views/ViewTemplate.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { FadeTransition } from '@/components/common';
+</script>
+<template>
+<RouterView v-slot="{ Component }">
+  <FadeTransition>
+    <component :is="Component"/>
+  </FadeTransition>
+</RouterView>
+</template>

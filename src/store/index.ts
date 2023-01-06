5562dac54dee0a457d1c77cc6063ff18b34f2e08
Cyrus <cyrus.kuznetsov@gmail.com>
2023-01-06 22:42:33 +0500

    refactor: remove test store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
 export { usePinia } from './pinia';
-export { useTestStore } from './test-store';
 export { useCartStore } from './cart/CartStore';
 export { usePaginationStore } from './cart/PaginationStore';
 export { usePromoStore } from './cart/PromoStore';

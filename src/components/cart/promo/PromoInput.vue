a993643cb2a18513b1c04675f64847f36da8eece
Nikita Kisly <sour945@gmail.com>
2023-01-08 12:49:53 +0300

    feat: add tooltip for promocode input in cart

--- src/components/cart/promo/PromoInput.vue
+++ src/components/cart/promo/PromoInput.vue
@@ -2,7 +2,13 @@
   <div class="summary__promo">
     <h4 class="summary__promo--header">Enter your promocode:</h4>
     <div class="promo__group">
-      <input type="text" placeholder="promocode" tooltip="Promocodes for test: gachi, 300bucks, rs-school" class="promo__group--input" v-model="promo" />
+      <input
+        type="text"
+        placeholder="promocode"
+        class="promo__group--input"
+        v-model="promo"
+      />
+      <span class="promo__group--tooltip"> Promocodes: <b>"gachi", "300bucks", "storeRS", "RS-school"</b></span>
       <my-button :class="{ disabled: !isMatch || isAlreadyApplied }" class="promo__group--apply" @click="applyPromo"
         >Apply</my-button
       >
@@ -35,6 +41,7 @@ const { applyPromo } = usePromoStore();
     display: flex;
     justify-content: space-between;
     gap: 1rem;
+    position: relative;
 
     &--input {
       padding: 0.3em 0.6rem;
@@ -48,6 +55,34 @@ const { applyPromo } = usePromoStore();
       &:focus-visible {
         transform: scale(0.96);
         outline-color: $primary;
+
+        & ~ .promo__group--tooltip {
+          opacity: 0;
+        }
+      }
+
+      &:hover ~ .promo__group--tooltip {
+        opacity: 1;
+      }
+    }
+
+    &--tooltip {
+      position: absolute;
+      padding: 0.5rem;
+      top: -190%;
+      background-color: $primary2-light;
+      border-radius: 10px;
+      box-shadow: 0px 0px 8px $dark;
+      opacity: 0;
+      transition: all 0.2s;
+
+      &::before {
+        content: '';
+        position: absolute;
+        left: 30%;
+        bottom: -20px;
+        border: 10px solid transparent;
+        border-top: 10px solid $primary2-light;
       }
     }
 

c75efd86407f26dcf66b522e8c08a12c5edee67a
Cyrus <cyrus.kuznetsov@gmail.com>
2023-01-09 13:55:28 +0500

    fix: add ignore ids

--- src/services/dummy-api-src.ts
+++ src/services/dummy-api-src.ts
@@ -1,14 +1,16 @@
-import { IProduct } from './model/product';
+import { IProduct } from './model';
 import { IProductSource } from './model/product-src';
 
 const apiUrl = 'https://dummyjson.com/products?limit=100';
+
+const ignoreIds = [24, 21, 22, 20, 29, 10, 1, 23, 89, 77, 56, 70, 66]
 
 export class DummyApi implements IProductSource {
   fetchProducts(): Promise<IProduct[]> {
     return fetch(apiUrl, { method: 'GET' })
       .then((data) => data.json())
       .then((item) => {
-        return item.products
+        return item.products.filter((product: IProduct) => !ignoreIds.includes(product.id))
       });
   }
 }

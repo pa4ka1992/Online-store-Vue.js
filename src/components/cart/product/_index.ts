ffc0d685a66c128e58f9f368c0d8b68dc9dacaeb
Nikita Kisly <sour945@gmail.com>
2023-01-30 10:24:50 +0300

    fix: fix imports for product components

--- src/components/cart/product/_index.ts
+++ src/components/cart/product/_index.ts
@@ -1,5 +1,3 @@
-import CartProductInfo from '@/components/cart/product/CartProductInfo.vue';
-import CartProductCountInfo from '@/components/cart/product/CartProductCountInfo.vue';
-import CartProductPrice from '@/components/cart/product/CartProductPrice.vue';
-
-export { CartProductInfo, CartProductCountInfo, CartProductPrice };
+export * from '@/components/cart/product/CartProductInfo.vue';
+export * from'@/components/cart/product/CartProductCountInfo.vue';
+export * from '@/components/cart/product/CartProductPrice.vue';
